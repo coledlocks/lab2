1a448c2aff030a73ce9dbc482f665c5099876886
coledlocks <cole.drozdeks@gmail.com>
2024-10-10 11:09:49 -0400

    Add files via upload

---
+++ lab2_module.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 10 11:00:36 2024
+lab2_module.py
+
+
+Cole Drozdek and Kaelen Kenna
+"""
+# importing related packages
+import sounddevice as sd
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy.io.wavfile import read
+
+#%% PART 1
+
+#%% PART 2
+
+#%% PART 3
+
+#%% PART 4
+
