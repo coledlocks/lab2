f14941ba1e02ed3cc4275086ebf0c3def8c0dc02
kaelenkenna <kaelen.kenna@uvm.edu>
2024-10-16 22:12:14 -0400

    Update lab2_module.py

--- lab2_module.py
+++ lab2_module.py
@@ -10,23 +10,23 @@ lab2_module.py
 import numpy as np
 from matplotlib import pyplot as plt
 
-#%% PART 2
-
+#%% PART 2: Build a Convolution Function
+# convolve input signals with system impulse
 def get_convolved_signal(input_signal, system_impulse):
-    # initiating the array
+    # array of zeros
     my_convolved_signal = np.zeros(len(input_signal) + len(system_impulse) - 1)
-    
-    # nested for loops
+    # loop through convolutions
     for in_sig_idx in range(len(input_signal)):
         for sys_imp_idx in range(len(system_impulse)):
             my_convolved_signal[in_sig_idx + sys_imp_idx] += input_signal[in_sig_idx] * system_impulse[sys_imp_idx]
     return my_convolved_signal
 
-#%% PART 3
-
+#%% PART 3: Simplify a Cascade System
+# calculate system output
 def run_drug_simulations(input_signal, system_impulse, dt, label):
-    y_t = np.convolve(input_signal, system_impulse)
+    system_output = np.convolve(input_signal, system_impulse)
     times = np.arange(0, len(y_t))/(1/dt)
+    # plot system output
     plt.figure(3)
-    plt.plot(times, y_t, label=label)
-
+    plt.plot(times, system_output, label=label)
+    
